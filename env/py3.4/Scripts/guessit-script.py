64fdc0d04c2d45df737db57182b1b2ab46152103
labrys <labrys@users.noreply.github.com>
2016-02-18 06:58:39 -0500

    init

---
+++ env/py3.4/Scripts/guessit-script.py
@@ -0,0 +1,10 @@
+#!C:\Src\lit-match\env\py3.4\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'guessit==2.0.4','console_scripts','guessit'
+__requires__ = 'guessit==2.0.4'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('guessit==2.0.4', 'console_scripts', 'guessit')()
+    )
